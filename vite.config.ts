450eb8cbffd645698345586be4556b53922ac6de
yceffort <yceffort@gmail.com>
2024-09-11 20:31:29 +0900

    🚀 init

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import {babel} from '@rollup/plugin-babel'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+import {defineConfig} from 'vite'
+
+import pkg from './package.json'
+
+const SUPPORT_TARGETS = browserslistToEsbuild()
+
+export default defineConfig({
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        useBuiltIns: 'usage',
+                        corejs: {version: 3, proposals: true},
+                    },
+                ],
+            ],
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            exclude: 'node_modules/**',
+        }),
+    ],
+    build: {
+        outDir: 'dist',
+        lib: {
+            entry: {
+                index: './src/index.ts',
+            },
+        },
+        rollupOptions: {
+            external: [...Object.keys(pkg.dependencies)].flatMap((dep) => [dep, new RegExp(`^${dep}/.*`)]),
+            output: [
+                {
+                    format: 'es',
+                    dir: 'dist',
+                },
+            ],
+        },
+        target: SUPPORT_TARGETS,
+    },
+})
